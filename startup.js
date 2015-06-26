c2e09dc59b642b16b86dd08f09dca8689da4befa
Erik Popp <epopp@i2pmail.org>
2015-06-26 12:35:51 -0500

    Started fixing the run-1-creature feature, and ran into a bug in Simulator.display().  They appear to be related.  I'm currently in the middle of adding debug code to Simulator.display()

--- startup.js
+++ startup.js
@@ -3,6 +3,6 @@
 
 var soup = new Array();	//create the array [soup]
 
-Simulator.blankCreature = Simulator.Creature("","transparent", 0);
+Simulator.blankCreature = Simulator.Creature("","rgb(255,255,255)", 0);
 var ancestor = Simulator.Creature("this.food++; soup.push( {source: this.source, food: 1, color: 'rgb(200,200,200)' } )", "rgb(200,200,200)", 1);
 soup.push( Simulator.Creature(ancestor.source, ancestor.color, ancestor.food) );
